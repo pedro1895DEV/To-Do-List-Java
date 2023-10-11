522d0d3f25283308ee6d74f040472aa9ea5983cf
pedro1895DEV <pedropaulomandrade@gmail.com>
2023-10-11 10:12:54 -0300

    feat: database integration

--- src/main/java/br/com/pedromartins/todolist/user/UserController.java
+++ src/main/java/br/com/pedromartins/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.pedromartins.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUserName(userModel.getUserName());
+
+        if(user != null){
+            System.err.println("Usuário já existe na base de dados");
+            return ResponseEntity.status(400).body("Usuário já existe na base de dados.");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(200).body("Usuário criado.");
     }
 }
